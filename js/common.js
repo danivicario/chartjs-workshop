b3d2eb5c913cef990757709aff1f3e3504c96068
Dani Vicario <danivicario@dvicario.es>
2020-02-13 07:42:09 +0100

    added

--- js/common.js
+++ js/common.js
@@ -3,12 +3,14 @@ const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min
 const shuffle = array => array.sort(() => Math.random() - 0.5);
 
 let chart;
+let chartData;
 const ctx = document.getElementById("myChart").getContext("2d");
 
 axios
   .get("http://localhost:8000/data")
   .then(responseFromAPI => {
-    printTheChart(responseFromAPI.data); // <== call the function here where you used to console.log() the response
+    chartData = responseFromAPI.data;
+    printTheChart(chartData); // <== call the function here where you used to console.log() the response
   })
   .catch(err => {
     console.log("Error while getting the data: ", err);
